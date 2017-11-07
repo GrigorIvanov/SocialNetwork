4d1660b10d1315d04d0ca55597b1175e04a70efa
Grigor Ivanov <grig0@abv.bg>
2017-11-07 10:28:39 +0200

    Wrong URL controller made

--- Book/src/main/java/com/example/WebInitializer.java
+++ Book/src/main/java/com/example/WebInitializer.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.
 AbstractAnnotationConfigDispatcherServletInitializer;
  
@@ -18,5 +20,12 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/", "*.html", "*.pdf" };
+    }
+    
+    @Override
+    protected DispatcherServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        final DispatcherServlet dispatcherServlet = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
     }
 }
